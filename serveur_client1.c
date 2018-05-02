57b589ac060ff905377c92f7af85da5e9742b235
angelep35 <38072753+angelep35@users.noreply.github.com>
2018-05-02 22:17:25 +0200

    Update serveur_client1.c

--- serveur_client1.c
+++ serveur_client1.c
@@ -1,6 +1,14 @@
 #include "serveur_client1.h"
 
 
+void sigfun(int sig)
+{
+    printf("Fermeture du serveur bye\n");
+    close(desc);
+    exit(0);
+	(void) signal(SIGINT, SIG_DFL);
+}
+
 int main(int argc, char* argv[]){
     clock_t temps=clock();
     if(argc != 2){
@@ -31,71 +39,84 @@ int main(int argc, char* argv[]){
 
             //bind
             printf("return bind : %d\n",bind(desc,(struct sockaddr*)&my_addr,sizeof(my_addr)));
-
-            //SYN client
-            char buffer[50];
-            int cli_len = sizeof(my_addr.sin_addr);
-            printf("En attente du SYN\n");
-            int rcv_syn = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
-            printf("SYN reçu\n");
-            if( rcv_syn <= 0 ) {
-                //error SYN
-                printf( "recvfrom() error \n" );
-                return -1;
-            }
-            printf("%s\n",buffer);
-            if(strcmp(buffer,"SYN") == 0){ //Si on a bien reçu SYN on continue 
-                printf("Envoie du synack avec nouveau port privé\n");
-                char syn_ack[12] = "SYN-ACK5009";
-                ssize_t sendsyn = sendto(desc,syn_ack,strlen(syn_ack),0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
-                int rcv_ack = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
-                if( rcv_ack <= 0 ) {
+            
+            while(1){
+                signal(SIGINT, sigfun);
+                //SYN client
+                char buffer[50];
+                int cli_len = sizeof(my_addr.sin_addr);
+                printf("En attente du SYN\n");
+                int rcv_syn = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
+                printf("SYN reçu\n");
+                if( rcv_syn <= 0 ) {
+                    //error SYN
                     printf( "recvfrom() error \n" );
                     return -1;
                 }
-                printf("%s\n",buffer); //on reçoit l'acquittement que le nouveau port à bien été reçu
-                if(strcmp(buffer,"ACK") == 0){
-                    int desc2;
-                    if( (desc2 = socket(PF_INET,SOCK_DGRAM,0)) > 0){ //on ouvre une deuxième socket privée
-                        int reuseok2;
-                        if( (reuseok2 = setsockopt(desc2,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse))) >= 0){
-
-                            //Struct sockaddr2 privé
-                            struct sockaddr_in my_addr2;
-                            memset((char*)&my_addr2,0,sizeof(my_addr2));
-                            my_addr2.sin_family = AF_INET;
-                            my_addr2.sin_port = htons(5009);
-                            my_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
-
-                            //Bind socket2
-                            bind(desc2,(struct sockaddr*)&my_addr2,sizeof(my_addr2));
-
-                            printf("Bind2\n");
-                            int rcv_mess = recvfrom(desc2, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr2.sin_addr, (unsigned*)&cli_len); //on reçoit le nom de fichier à envoyer
-                            if( rcv_mess <= 0 ) {
-                                printf( "recvfrom() error \n" );
-                                return -1;
-                            }
-                            printf("reçu nom fichier : %s\n",buffer);
-
-                            slowStart(buffer,desc2,my_addr2,cli_len);
-                            
-
-
-//END !!!!!
-
-                        }else{printf("erreur setsockopt 2");return -1;}
-                    } else{printf("erreur création socket 2");return-1;}
+                printf("%s\n",buffer);
+                if(strcmp(buffer,"SYN") == 0){ //Si on a bien reçu SYN on continue 
                     
-                }
-            }
+                    printf("Envoie du synack avec nouveau port privé\n");
+                    numport++;
+                    char syn_ack[20] = "SYN-ACK";
+                    char port[4];
+                    sprintf(port, "%d", numport);
+                    strcat(syn_ack, port);
+                    ssize_t sendsyn = sendto(desc,syn_ack,strlen(syn_ack),0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
+                    int rcv_ack = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
+                    if( rcv_ack <= 0 ) {
+                        printf( "recvfrom() error \n" );
+                        return -1;
+                    }
+                    printf("%s\n",buffer); //on reçoit l'acquittement que le nouveau port à bien été reçu
+                    if(strcmp(buffer,"ACK") == 0){
+                        printf("Ouverture deuxième socket\n");
+                        int desc2;
+                        if( (desc2 = socket(PF_INET,SOCK_DGRAM,0)) > 0){ //on ouvre une deuxième socket privée
+                            int reuseok2;
+                            if( (reuseok2 = setsockopt(desc2,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse))) >= 0){
 
+                                //Struct sockaddr2 privé
+                                struct sockaddr_in my_addr2;
+                                memset((char*)&my_addr2,0,sizeof(my_addr2));
+                                my_addr2.sin_family = AF_INET;
+                                my_addr2.sin_port = htons(numport);
+                                my_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
+
+                                //Bind socket2
+                                bind(desc2,(struct sockaddr*)&my_addr2,sizeof(my_addr2));
+                                printf("Bind2\n");
+                                int pid = fork();
+                                if(pid == 0){
+
+                                    printf("je suis dans le fils\n");
+                                    close(desc);
+                                    sleep(1);
+                                    int rcv_mess = recvfrom(desc2, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr2.sin_addr, (unsigned*)&cli_len); //on reçoit le nom de fichier à envoyer
+                                    if( rcv_mess <= 0 ) {
+                                        printf( "recvfrom() error \n" );
+                                        return -1;
+                                    }
+                                    printf("reçu nom fichier : %s\n",buffer);
+
+                                    slowStart(buffer,desc2,my_addr2,cli_len);
+                                    exit(0);
+                                    close(desc2);
+                                }
+                            }else{printf("erreur setsockopt 2");return -1;}
+                        } else{printf("erreur création socket 2");return-1;}
+                        close(desc2);
+                    }else{printf("mauvaise réception de l'ACK");return-1;}
+                        
+                }else{printf("mauvaise réception du SYN");return-1;}
+            }       
         }else{printf("erreur setsockopt 1");return -1;}
 
         close(desc);
         clock_t temps2=clock();
         printf("\n diff de temp : %lf\n",(double)(temps2-temps)/CLOCKS_PER_SEC);
         return 1;
+    
     }else{printf("erreur création socket 1");return-1;}
 }
 
@@ -189,6 +210,7 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
     
     while(nbelemrecu==nbElements){ //tant que ce n'est pas la fin du fichier
         compt=0;
+        if (cwnd==0) cwnd++;
         while(compt<cwnd){ //on envoie cwnd paquets
             seq++;
             ajoutSeq(seq,messEnvoye); //on garde le numero de la dernière sequence
@@ -240,6 +262,8 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
         //printf("\n");
     }
     ssize_t sendLine=sendto(desc,"FIN",4,0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
+    fclose(f);
     return nbelemrecu;
+
     
 }
