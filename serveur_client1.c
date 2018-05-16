58a8bfa1b50e0bde219afd717cb3fca7b48785b9
angelep35 <38072753+angelep35@users.noreply.github.com>
2018-05-16 10:23:30 +0200

    Update serveur_client1.c

--- serveur_client1.c
+++ serveur_client1.c
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]){
                 //SYN client
                 char buffer[50];
                 int cli_len = sizeof(my_addr.sin_addr);
-                printf("En attente du SYN pid : %d\n",getpid());
+                printf("En attente du SYN\n");
                 int rcv_syn = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
                 printf("SYN reçu\n");
                 if( rcv_syn <= 0 ) {
@@ -55,49 +55,43 @@ int main(int argc, char* argv[]){
                 }
                 printf("%s\n",buffer);
                 if(strcmp(buffer,"SYN") == 0){ //Si on a bien reçu SYN on continue 
-                    numport++;
-
-                    printf("Envoie du synack avec nouveau port privé\n");
                     
+                    printf("Envoi du synack avec nouveau port privé\n");
+                    numport++;
                     char syn_ack[20] = "SYN-ACK";
                     char port[4];
                     sprintf(port, "%d", numport);
                     strcat(syn_ack, port);
-                    ssize_t sendsyn = sendto(desc,syn_ack,sizeof(syn_ack),0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
+                    ssize_t sendsyn = sendto(desc,syn_ack,strlen(syn_ack),0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
                     int rcv_ack = recvfrom( desc, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
-
                     if( rcv_ack <= 0 ) {
                         printf( "recvfrom() error \n" );
                         return -1;
                     }
                     printf("%s\n",buffer); //on reçoit l'acquittement que le nouveau port à bien été reçu
-
                     if(strcmp(buffer,"ACK") == 0){
                         printf("Ouverture deuxième socket\n");
-                        
+                        int desc2;
+                        if( (desc2 = socket(PF_INET,SOCK_DGRAM,0)) > 0){ //on ouvre une deuxième socket privée
+                            int reuseok2;
+                            printf("desc2=%d\n",desc2);
+                            if( (reuseok2 = setsockopt(desc2,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse))) >= 0){
 
-                        int pid = fork();
+                                //Struct sockaddr2 privé
+                                struct sockaddr_in my_addr2;
+                                memset((char*)&my_addr2,0,sizeof(my_addr2));
+                                my_addr2.sin_family = AF_INET;
+                                my_addr2.sin_port = htons(numport);
+                                my_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
 
-                        if(pid == 0){
-                            printf("je suis dans le fils port : %d\n",numport);
-                            int desc2;
-                            if( (desc2 = socket(PF_INET,SOCK_DGRAM,0)) > 0){ //on ouvre une deuxième socket privée
-                                int reuseok2;
-                                if( (reuseok2 = setsockopt(desc2,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse))) >= 0){
+                                //Bind socket2
+                                bind(desc2,(struct sockaddr*)&my_addr2,sizeof(my_addr2));
+                                printf("Bind2\n");
+                                int pid = fork();
+                                if(pid == 0){
 
-                                    //Struct sockaddr2 privé
-                                    struct sockaddr_in my_addr2;
-                                    memset((char*)&my_addr2,0,sizeof(my_addr2));
-                                    my_addr2.sin_family = AF_INET;
-                                    my_addr2.sin_port = htons(numport);
-                                    my_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
-
-                                    
-
-                                    //Bind socket2
-                                    if(bind(desc2,(struct sockaddr*)&my_addr2,sizeof(my_addr2))==0){
-                                    printf("Bind2 au port %d\n",numport);
-                                    }                                
+                                    printf("je suis dans le fils\n");
+                                    sleep(1);
                                     int rcv_mess = recvfrom(desc2, buffer, sizeof(buffer), 0,(struct sockaddr*)&my_addr2.sin_addr, (unsigned*)&cli_len); //on reçoit le nom de fichier à envoyer
                                     if( rcv_mess <= 0 ) {
                                         printf( "recvfrom() error \n" );
@@ -109,13 +103,10 @@ int main(int argc, char* argv[]){
                                     close(desc2);
                                     exit(0);
 
-                                    
-                                
-                                }else{printf("erreur setsockopt 2");return -1;}
-                        
-                            }else{printf("erreur création socket 2");return-1;}
-                            close(desc2);
-                        }
+                                }
+                            }else{printf("erreur setsockopt 2");return -1;}
+                        } else{printf("erreur création socket 2");return-1;}
+                        close(desc2);
                     }else{printf("mauvaise réception de l'ACK");return-1;}
                         
                 }else{printf("mauvaise réception du SYN");return-1;}
@@ -123,7 +114,8 @@ int main(int argc, char* argv[]){
         }else{printf("erreur setsockopt 1");return -1;}
 
         close(desc);
-
+        clock_t temps2=clock();
+        printf("\n diff de temp : %lf\n",(double)(temps2-temps)/CLOCKS_PER_SEC);
         return 1;
     
     }else{printf("erreur création socket 1");return-1;}
@@ -173,8 +165,9 @@ int envoiImage(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
         ajoutSeq(seq,messEnvoye);
         printf("mess=%s\n",messEnvoye);
         nbelemrecu = fread( buffer , sizeof(char) , nbElements , f);
-        
-    
+        /*for(int i=0;i<nbelemrecu;i++){ //copie du buffer dans le message à envoyer
+            messEnvoye[i+6]=buffer[i];  //à améliorer car perte de temps mais j'arrivais pas avec strcat
+        }*/
         memcpy(messEnvoye+6,buffer,nbelemrecu);
         ssize_t sendLine=sendto(desc,messEnvoye,nbelemrecu+6,0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
         printf("renvoie du sendLine : %d\n", (int)sendLine);
@@ -201,7 +194,6 @@ int envoiImage(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
 
 
 int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
-    printf("slow start");
     FILE *f = fopen(file,"rb");
     int nbElements = 1493;
     char buffer[nbElements];
@@ -213,7 +205,7 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
     int max=0;
     int numSeq;
     int cwnd=1;
-    int ssthresh=100;
+    int ssthresh=130;
     int compt;
 
     
@@ -234,17 +226,15 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
             //printf("Message %d sur %d envoyé\n",compt,cwnd);
         }
 
-        struct timeval temps_avant, temps_apres, diff; //on lance un timer
- 
-        gettimeofday (&temps_avant, NULL);
-    
-        //while(((clock()/CLOCKS_PER_SEC)-temps)<0.01f && max<seq){ //tant qu'on est pas en timeout et qu'on à pas reçu le plus grand ack
-        do{    
+        temps=clock()/CLOCKS_PER_SEC; //on lance un timer
+        while(((clock()/CLOCKS_PER_SEC)-temps)<0.01f && max<seq){ //tant qu'on est pas en timeout et qu'on à pas reçu le plus grand ack
             int rcv_ack = recvfrom(desc, ackRecu, sizeof(ackRecu),MSG_DONTWAIT,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
+            
+            //implémenter fast recovery
+
             int numAck=ackToInt(ackRecu);
             max=getMax(numAck,max);
-            gettimeofday (&temps_apres, NULL);
-        }while((((temps_apres.tv_sec - temps_avant.tv_sec) * 1000000 + temps_apres.tv_usec) - temps_avant.tv_usec)<2000 && max<seq);
+        }
 
         if(max==seq){ //si on à recu l'ack du dernier paquet envoyé
             if(cwnd>=ssthresh) { //si on est en congestion avoidance
