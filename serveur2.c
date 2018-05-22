fba090bb1efcf5721882daadb1b80a254cd91fe4
angelep35 <38072753+angelep35@users.noreply.github.com>
2018-05-22 19:44:45 +0200

    Update serveur2.c

--- serveur2.c
+++ serveur2.c
@@ -1,6 +1,6 @@
 #include "serveur2.h"
 
-#define TAILLE_PAQUET 1490
+#define TAILLE_PAQUET 1494
 #define ELEM_RESTANT tailleFichier-nbPaquet*TAILLE_PAQUET
 
 
@@ -8,7 +8,6 @@
 
 int main(int argc, char* argv[]){
     if(argc != 2){
-        printf("Arguments -- Numéro de port nécessaire ou arguments erronés\n");
         exit(0);
     }
 
@@ -27,13 +26,10 @@ int main(int argc, char* argv[]){
 
     //Création Socket
     if ( (desc_connect = socket(PF_INET,SOCK_DGRAM,0)) > 0 ){
-        printf("Socket connexion réussi\n");
 
         //Bind Socket Connect
         if(bind(desc_connect,(struct sockaddr*)&sa_connect,sizeof(sa_connect)) == 0){
-            printf("Bind Connexion réussi\n");
         }else{
-            printf("Erreur dans le bind connexion client");
             closing(desc_client,desc_connect);
             exit(0);
         }
@@ -47,16 +43,12 @@ int main(int argc, char* argv[]){
             //SYN Client
             char buffer[50];
             int cli_len = sizeof(sa_connect.sin_addr);
-            printf("En attente du SYN\n"); 
             if( recvfrom( desc_connect, buffer, sizeof(buffer), 0,(struct sockaddr*)&sa_connect.sin_addr, (unsigned*)&cli_len) <= 0){
-                printf("Erreur reception SYN\n");
                 exit(0);
             }
-            printf("%s reçu\n",buffer);
 
             //Si SYN reçu on continu
             if(strcmp(buffer,"SYN") == 0){
-                printf("Envoi de SYN-ACK avec nouveau port\n");
                 port_client++;
                 char syn_ack[20] = "SYN-ACK";
                 char port[4];
@@ -64,13 +56,10 @@ int main(int argc, char* argv[]){
                 strcat(syn_ack, port);
                 sendto(desc_connect,syn_ack,strlen(syn_ack),0,(struct sockaddr*)&sa_connect.sin_addr, cli_len);
                 if(recvfrom( desc_connect, buffer, sizeof(buffer), 0,(struct sockaddr*)&sa_connect.sin_addr, (unsigned*)&cli_len) <= 0){
-                    printf("Erreur réception ACK\n");
 
                 }
-                printf("%s reçu\n",buffer);
                 //Si ACK reçu on continu
                 if(strcmp(buffer,"ACK") == 0){
-                        printf("Ouverture Socket Client\n");
                 }
                 //Création Socket Client
                 if( (desc_client = socket(PF_INET,SOCK_DGRAM,0)) > 0){
@@ -85,18 +74,14 @@ int main(int argc, char* argv[]){
                         sa_client.sin_addr.s_addr = htonl(INADDR_ANY);
 
                         bind(desc_client,(struct sockaddr*)&sa_client,sizeof(sa_client));
-                        printf("Bind Client réussi sur port %d\n",port_client);
 
                         //Création processus fils
                         int pid = fork();
                         if(pid == 0){
-                            printf("Processus créé -- PID = %d\n",getpid());
 
                             //Réception du nom de fichier
                             if( recvfrom(desc_client, buffer, sizeof(buffer), 0,(struct sockaddr*)&sa_client.sin_addr, (unsigned*)&cli_len) <= 0){
-                                printf("Erreur reception nom fichier\n");
                             }
-                            printf("Fichier demandé ; %s\n",buffer);
 
                             //Envoi du fichier
                             slowStart(buffer,desc_client,sa_client,cli_len);
@@ -112,7 +97,6 @@ int main(int argc, char* argv[]){
                     }
 
                 }else{
-                    printf("Erreur création Socket Client\n");
                     closing(desc_client,desc_connect);
                     exit(0);
                 }
@@ -127,7 +111,6 @@ int main(int argc, char* argv[]){
 
 
     }else{
-        printf("Erreur dans la création de la socket de connection\n");
         closing(desc_client,desc_connect);
         exit(0);
     }
@@ -144,7 +127,6 @@ int main(int argc, char* argv[]){
 
 //Fonction détournement signal Ctrl-C
 void sigfun(int sig){
-    printf("Fermeture du serveur -- PID = %d\n",getpid());
     closing(desc_client,desc_connect);
     exit(0);
     (void) signal(SIGINT, SIG_DFL);
@@ -166,17 +148,14 @@ void ajoutSeq (int seq, char* messEnvoye){
 }
 
 int envoiImage(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
-    printf("Envoi Image\n");
     
     //Ouverture du fichier en binaire
     FILE *f = fopen(file,"rb");
     fseek (f, 0, SEEK_END);   // non-portable
     int tailleFichier=ftell (f);
-    printf("Taille fichier : %d\n",tailleFichier);
     char bufferfile[tailleFichier];
     fseek(f, 0, SEEK_SET);
     int lect=fread(bufferfile,1,sizeof(bufferfile),f);
-    printf("Fichier stocké en mémoire -- size : %d",lect);
 
     //Initialisation paramètre
     int seq=0;
@@ -231,18 +210,16 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
     clock_t temps=0;
     int max=0;
     int numSeq;
-    int cwnd=20;
+    int cwnd=185;
     int ssthresh=130;
     int compt;
     
     FILE *f = fopen(file,"rb");
     fseek (f, 0, SEEK_END);   // non-portable
     int tailleFichier=ftell (f);
-    printf("Taille fichier : %d\n",tailleFichier);
-    char bufferfile[tailleFichier];
+    char *bufferfile = malloc(tailleFichier);
     fseek(f, 0, SEEK_SET);
     int lect=fread(bufferfile,1,sizeof(bufferfile),f);
-    printf("Fichier stocké en mémoire -- size : %d\n",lect);
     int fin=0;
     int seqmax=1+tailleFichier/TAILLE_PAQUET;
     
@@ -271,7 +248,7 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
         }
 
         temps=clock()*1000000/CLOCKS_PER_SEC; //on lance un timer
-        while(((clock()*1000000/CLOCKS_PER_SEC)-temps)<(3000*cwnd) && max<seq){ //tant qu'on est pas en timeout et qu'on à pas reçu le plus grand ack
+        while(((clock()*1000000/CLOCKS_PER_SEC)-temps)<(300*cwnd) && max<seq){ //tant qu'on est pas en timeout et qu'on à pas reçu le plus grand ack
             
             int rcv_ack = recvfrom(desc, ackRecu, sizeof(ackRecu),MSG_DONTWAIT,(struct sockaddr*)&my_addr.sin_addr, (unsigned*)&cli_len);
             int numAck=ackToInt(ackRecu);
@@ -286,30 +263,30 @@ int slowStart(char* file,int desc,  struct sockaddr_in my_addr, int cli_len){
         }
         if(max==seq){ //si on à recu l'ack du dernier paquet envoyé
             if(cwnd>=ssthresh) { //si on est en congestion avoidance
-                if(cwnd<20){cwnd+=40;}
-                else { cwnd*=2;}
+               // if(cwnd<20){cwnd+=40;}
+                //else { cwnd*=2;}
             } else {
-                if(cwnd<20){cwnd+=40;}
-                else { cwnd*=2;}
+                //if(cwnd<20){cwnd+=40;}
+                //else { cwnd*=2;}
             }
         } else if (max>seq){ //si on recoit un ack du groupe de message d'avant
             int diffAck=seq-max;
             seq-=diffAck;
             nbPaquet-=diffAck;
             if(cwnd>ssthresh) { //si on est en congestion avoidance
-                if(cwnd<20){cwnd+=40;}
-                else { cwnd*=2;}
+                //if(cwnd<20){cwnd+=40;}
+                //else { cwnd*=2;}
             } else {
-                if(cwnd<20){cwnd+=40;}
-                else { cwnd*=2;}
+                //if(cwnd<20){cwnd+=40;}
+                //else { cwnd*=2;}
             }
         }else {
             int nbAckNonRecu=seq-max;
             seq-=nbAckNonRecu;
             nbPaquet-=nbAckNonRecu;
             ssthresh=cwnd/1.7; //on fixe le threshold pr congestion avoidance
-            if(cwnd>3) cwnd-=3;
-        } printf("cwnd=%d\n",cwnd);
+            //if(cwnd>3) cwnd-=3;
+        } 
     }
     ssize_t sendLine=sendto(desc,"FIN",4,0,(struct sockaddr*)&my_addr.sin_addr, cli_len);
     fclose(f);
